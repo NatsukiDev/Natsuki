2ea43f7cedd98e84f578a3771a3225b4e830e89f
WubzyGD <wubzygd@gmail.com>
2020-11-15 01:05:59 -0700

    Client reloading; n?reload

--- handle/command.js
+++ handle/command.js
@@ -6,10 +6,11 @@ module.exports = client => {
     var commands = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
     console.log(`\n${chalk.gray('[BOOT]')} >> ${chalk.blue('Getting Commands...')}\n`);
     for (let commandf of commands) {
+        if (Object.keys(require.cache).includes(require.resolve(`../commands/${commandf}`))) {delete require.cache[require.resolve(`../commands/${commandf}`)];}
         var command = require(`../commands/${commandf}`);
         client.commands.set(command.name, command);
         if (command.aliases) {command.aliases.forEach(a => client.aliases.set(a, command.name));}
-        console.log(`${chalk.gray('[LOG] ')} >> ${chalk.blueBright('Loaded Command')} ${chalk.white(command.name)}`);
+        console.log(`${chalk.gray('[LOG] ')} >> ${chalk.blueBright('Loaded Command')} ${chalk.white(command.name)} ${chalk.blueBright('with')} ${chalk.white(command.aliases && command.aliases.length ? command.aliases.length : 0)} ${chalk.blueBright('aliases')}`);
     }
     console.log(`\n${chalk.gray('[BOOT]')} >> ${chalk.blue('Loaded all Commands')}`);
 };
--- handle/event.js
+++ handle/event.js
@@ -6,8 +6,10 @@ module.exports = client => {
     let eventFilter = fs.readdirSync('./events/').filter(x => x.endsWith('.js'));
     console.log(`\n${chalk.gray('[BOOT]')} >> ${chalk.blue('Getting Events...')}\n`);
     for (let file of eventFilter) {
-        let evt = require('../events/' + file);
         let evtName = file.split('.')[0];
+        if (Object.keys(require.cache).includes(require.resolve('../events/' + file))) {delete require.cache[require.resolve('../events/' + file)];}
+        let evt = require('../events/' + file);
+        client.removeAllListeners(evtName);
         client.on(evtName, evt.bind(null, client));
         console.log(`${chalk.gray('[LOG] ')} >> ${chalk.blueBright('Loaded Event')} ${chalk.white(evtName)}`);
     }
