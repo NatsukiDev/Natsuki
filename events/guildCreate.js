4c6588455c696e2586547748a53973f1c1bc1259
WubzyGD <63122940+WubzyGD@users.noreply.github.com>
2020-12-11 05:57:47 -0500

    Fix guild add logging

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -15,12 +15,12 @@ module.exports = async (client, guild) => {
     botData.save();
 
     client.guilds.cache.get('762707532417335296').channels.cache.get('766031709866557471').send(new Discord.MessageEmbed()
-        .setAuthor('New Guild Added', guild.owner.avatarURL())
+        .setAuthor('New Guild Added', client.users.cache.get(guild.owner.id).avatarURL())
         .setTitle(guild.name)
         .setThumbnail(guild.iconURL({size: 2048}))
-        .addField('Owner', guild.owner.tag, true)
-        .addField('Members', guild.members.cache.size)
-        .addField('Position', `Server #${client.guilds.cache.size}`)
+        .addField('Owner', client.users.cache.get(guild.owner.id).tag, true)
+        .addField('Members', guild.members.cache.size, true)
+        .addField('Position', `Server #${client.guilds.cache.size}`, true)
         .setColor('55ff7f')
         .setFooter('Natsuki')
         .setTimestamp()
