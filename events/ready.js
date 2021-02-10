5025614ff6205c549e7ed0ee54ea592beb968d4a
WubzyGD <wubzygd@gmail.com>
2021-02-10 11:03:32 -0700

    Improve startup

--- events/ready.js
+++ events/ready.js
@@ -13,14 +13,6 @@ var prefix = 'n?';
 
 module.exports = async client => {
 	const config = client.config;
-	try {
-		await mongoose.connect(`mongodb+srv://${config.database.user}:${config.database.password}@${config.database.cluster}.3jpp4.mongodb.net/test`, {
-			useFindAndModify: false, useNewUrlParser: true, dbName: 'Natsuki-Main', useUnifiedTopology: true, useCreateIndex: true
-		});
-	} catch (e) {
-		let date = new Date; date = date.toString().slice(date.toString().search(":") - 2, date.toString().search(":") + 6);
-		console.error(`\n${chalk.red('[ERROR]')} >> ${chalk.yellow(`At [${date}] | Occurred while trying to connect to Mongo Cluster`)}`, e);
-	}
 
 	/*let db = mongoose.connection;
 	await db.guild.update({}, {"$set": {'prefix': ''}}, false, true);*/
