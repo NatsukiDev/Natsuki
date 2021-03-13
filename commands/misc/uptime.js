d4fdb5611507a1f48d0036375c178f3498ef20d0
WubzyGD <wubzygd@gmail.com>
2021-03-13 16:09:58 -0700

    add uptime cmd

---
+++ commands/misc/uptime.js
@@ -0,0 +1,29 @@
+const Discord = require('discord.js');
+
+const Bot = require('../../models/bot');
+
+const moment = require('moment');
+require('moment-precise-range-plugin');
+
+module.exports = {
+    name: "uptime",
+    aliases: ['ut', 'up'],
+    meta: {
+        category: 'Misc',
+        description: "Shows the bot's uptime",
+        syntax: '`uptime`',
+        extra: null
+    },
+    help: "Shows my uptime, which is how long it's been since my last restart.",
+    async execute(message, msg, args, cmd, prefix, mention, client) {
+        const bot = await Bot.findOne({finder: 'lel'});
+        return message.channel.send(new Discord.MessageEmbed()
+            .setTitle("Uptime")
+            .setThumbnail(client.user.avatarURL({size: 2048}))
+            .setDescription(moment.preciseDiff(moment(bot.lastRestart), moment()))
+            .setColor('c375f0')
+            .setFooter("Natsuki")
+            .setTimestamp()
+        )
+    }
+};
