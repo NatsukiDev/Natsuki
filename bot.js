580b11c0faab63da52b3aa17bf1538d30093c048
WubzyGD <wubzygd@gmail.com>
2021-03-28 05:43:34 -0600

    add n?pat

--- bot.js
+++ bot.js
@@ -65,7 +65,7 @@ async function init() {
 
     ['command', 'event', 'response'].forEach(x => require(`./handle/${x}`)(client));
 
-    client.developers = ["330547934951112705", "673477059904929802"];
+    client.developers = ["330547934951112705", "673477059904929802", "468903364533420074"];
     client.utils = {};
 
     client.utils.logch = async () => {return client.guilds.cache.get('762707532417335296').channels.cache.get('762732961753595915');};
