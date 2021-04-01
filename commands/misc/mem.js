5d0a17690e2ff000dcf955ac266856a6c24e9d8e
WubzyGD <wubzygd@gmail.com>
2021-03-31 20:26:21 -0600

    mor bug

--- commands/misc/mem.js
+++ commands/misc/mem.js
@@ -13,7 +13,7 @@ module.exports = {
     async execute(message, msg, args, cmd, prefix, mention, client) {
         return message.channel.send(new Discord.MessageEmbed()
             .setTitle("RAM Usage")
-            .setDescription("Mem", `\`${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB\` heap of \`${(process.memoryUsage().heapTotal / 1024 / 1024).toFixed(2)}MB\` allocated. | **${Math.floor((process.memoryUsage().heapUsed / process.memoryUsage().heapTotal) * 100)}%**\nTotal RAM: \`${(os.totalmem() / 1024 / 1024 / 1024).toFixed(2)}GB\` | Free RAM: \`${(os.freemem() / 1024 / 1024 / 1024).toFixed(2)}GB\``)
+            .setDescription(`\`${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB\` heap of \`${(process.memoryUsage().heapTotal / 1024 / 1024).toFixed(2)}MB\` allocated. | **${Math.floor((process.memoryUsage().heapUsed / process.memoryUsage().heapTotal) * 100)}%**\nTotal RAM: \`${(os.totalmem() / 1024 / 1024 / 1024).toFixed(2)}GB\` | Free RAM: \`${(os.freemem() / 1024 / 1024 / 1024).toFixed(2)}GB\``)
             .setColor('c375f0')
         );
     }
