0e1480f9d9607a2672f79c85a9fc6f109f3437f7
WubzyGD <wubzygd@gmail.com>
2021-12-18 21:34:50 -0700

    attachment url for ask.js

--- util/ask.js
+++ util/ask.js
@@ -4,6 +4,7 @@ module.exports = async (message, toAsk, time, nofilter, timeoutResponse) => {
     try {
         let collected = await msg.channel.awaitMessages({filter: filter, max: 1, errors: ['time'], time: time});
         collected = collected.first().content;
+        if (collected.first().attachments.size) {collected = collected.first().attachments.first().url;}
         return collected;
     } catch {
         if (timeoutResponse) {message.reply("This question has timed out! Please try again.");}
