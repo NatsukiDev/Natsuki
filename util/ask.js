50429ce861da3e6dad87e95ef6088d0ab45447f8
WubzyGD <wubzygd@gmail.com>
2021-12-20 03:29:42 -0700

    raw images now actually work in ask.js

--- util/ask.js
+++ util/ask.js
@@ -3,8 +3,8 @@ module.exports = async (message, toAsk, time, nofilter, timeoutResponse) => {
     let filter = nofilter ? () => true : m => m.author.id === message.author.id;
     try {
         let collected = await msg.channel.awaitMessages({filter: filter, max: 1, errors: ['time'], time: time});
-        collected = collected.first().content;
-        //if (collected.first().attachments.size) {collected = collected.first().attachments.first().url;}
+        if (collected.first().attachments.size > 0) {collected = collected.first().attachments.first().url;}
+        else {collected = collected.first().content;}
         return collected;
     } catch {
         if (timeoutResponse) {message.reply("This question has timed out! Please try again.");}
