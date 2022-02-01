90981829aa4591fa04da0be90db0c3d0bed3b59b
WubzyGD <wubzygd@gmail.com>
2022-01-31 18:21:48 -0700

    remove useless info from userinfo

--- commands/misc/userinfo.js
+++ commands/misc/userinfo.js
@@ -36,9 +36,9 @@ module.exports = {
         }
 
         if (tu) {
-            infoembed.addField('Natsuki Commands Executed', `${tu.commands}`)
-                .addField('Donator?', tu.developer ? `Well, ${name} makes me work, so they're a supporter in my book!` : tu.donator ? 'Yes! They have donated or supported me in the past!' : 'No', true)
-                .addField('Natsuki Staff Level', tu.developer ? 'Developer' : tu.admin ? 'Admin; Audit access to the bot' : tu.staff ? 'Staff; Support but with maintenance permissions' : tu.support ? 'Support; Answers tickets and help queries' : 'Member; Does not have a staff rank.', true);
+            infoembed.addField('Natsuki Commands Executed', `${tu.commands}`);
+            if (tu.donator || tu.developer) {infoembed.addField('Donator?', tu.developer ? `Well, ${name} makes me work, so they're a supporter in my book!` : tu.donator ? 'Yes! They have donated or supported me in the past!' : 'No', true);}
+            if (tu.staff) {infoembed.addField('Natsuki Staff Level', tu.developer ? 'Developer' : tu.admin ? 'Admin; Audit access to the bot' : tu.staff ? 'Staff; Support but with maintenance permissions' : tu.support ? 'Support; Answers tickets and help queries' : 'Member; Does not have a staff rank.', true);}
         }
         return message.channel.send({embeds: [infoembed]});
     }
