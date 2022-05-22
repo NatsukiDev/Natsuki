8c10ba4f5697a20a4cb08717771277842003cda5
WubzyGD <wubzygd@gmail.com>
2022-05-22 02:50:10 -0500

    avatar bug

--- commands/misc/avatar.js
+++ commands/misc/avatar.js
@@ -16,8 +16,8 @@ module.exports = {
     },
     async execute(message, msg, args, cmd, prefix, mention, client) {
         let fail = false;
-        await client.users.fetch(member.id, {force: true}).catch(() => fail = true);
         let member = args.length && !fail ? (mention || client.users.cache.get(args[0]) || message.author) : message.author;
+        await client.users.fetch(member.id, {force: true}).catch(() => fail = true);
         if (message.guild) {message.guild.members.fetch(member.id, {force: true});}
         let name = message.guild ? message.guild.members.cache.get(member.id).displayName : member.username;
         let options = new TagFilter([
