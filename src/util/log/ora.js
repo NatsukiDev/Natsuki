2dadce739117cd84126e79fa11885df95f778642
WubzyGD <wubzygd@gmail.com>
2022-10-27 02:13:15 -0600

    ora promise wrap

--- src/util/log/ora.js
+++ src/util/log/ora.js
@@ -1,12 +1,18 @@
 const ora = require('ora');
 
 module.exports = async (text, promise, callback) => {
-    let spin = ora(text).start();
-    await promise
-        .catch(e => callback(spin, e))
-        .then((...args) => {
-            spin.stop();
-            spin.clear();
-            callback(spin, args);
-        });
+    return new Promise(async (resolve, reject) => {
+        let spin = ora(text).start();
+        return await promise
+            .catch(e => {
+                spin.stop();
+                spin.clear();
+                return reject(e)
+            })
+            .then((...args) => {
+                spin.stop();
+                spin.clear();
+                return callback ? resolve(callback(spin, args)) : resolve(true);
+            });
+    });
 };
