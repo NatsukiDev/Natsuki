8018ff8dd95a5510ae7767d585df92298e41df2e
WubzyGD <wubzygd@gmail.com>
2022-11-03 22:21:07 -0600

    skeleton the bot!

--- bot.js
+++ bot.js
@@ -1,5 +1,16 @@
-const dbConnect = require('./src/db/connect');
-const r = async () => {
-    await dbConnect({auth: require('./src/json/auth.json'), config: {logLevel: 1}});
+const Discord = require('discord.js');
+
+const auth = require('./src/json/auth.json');
+const config = require('./src/json/config.json');
+
+
+const flags = Discord.Intents.FLAGS;
+let fl = []; Object.keys(flags).forEach(flag => fl.push(flags[flag])); // fuck new standards i'm in't'zing with all the flags.
+const client = new Discord.Client({intents: fl, partials: ["CHANNEL", "REACTION", "MESSAGE"]});
+
+const startBot = async () => {
+    client.config = config;
+    client.auth = auth;
 };
-r();
+startBot();
+// feels like there isn't a function name to do this justice :joy:
