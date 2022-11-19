ca181acc425c8c61651de072a6fb864ac6e7c736
WubzyGD <wubzygd@gmail.com>
2022-11-19 03:41:10 -0700

    status setting

--- src/handle/runtime/events/ready.js
+++ src/handle/runtime/events/ready.js
@@ -1,5 +1,8 @@
 const Discord = require('discord.js');
 
 module.exports = async client => {
+    client.prefix = "n?";
+
     require('../../startup/run/hello')(client); // startup info
+    require('../../startup/run/setstatus')(client);
 };
